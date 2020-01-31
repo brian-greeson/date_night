cfcad6a011e46663abe3ed597da7ba14b69c3915
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-01-30 18:31:24 -0700

    Add it_can_create_a_node test

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,7 +1,12 @@
 class BinarySearchTree
 
   def initialize()
-    
+
+  end
+
+  def insert(score = 0, title = "")
+    node = Node.new(score, title)
+    node.depth
   end
 
 end
--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -1,6 +1,7 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/binary_search_tree"
+require "./lib/node"
 
 require "pry"
 
@@ -10,6 +11,12 @@ class BinarySearchTreeTest < Minitest::Test
     tree = BinarySearchTree.new
 
     assert_instance_of BinarySearchTree, tree
+  end
+
+  def test_it_can_create_a_node
+    tree = BinarySearchTree.new
+    tree.insert(61, "Bill & Ted's Excellent Adventure")
+    
   end
 
 end
