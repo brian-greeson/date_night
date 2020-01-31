14c13339d269f8483bf68b94c94012bfb6a4f386
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-01-31 08:49:55 -0700

    Update node class with depth attribute and test

--- lib/node.rb
+++ lib/node.rb
@@ -1,12 +1,13 @@
 
 class Node
   attr_reader :name, :score
-  attr_accessor :left, :right
-  def initialize(score = 0, name = "")
+  attr_accessor :left, :right, :depth
+  def initialize(score = 0, name = "", depth = 0)
     @name = name
     @score = score
     @left = nil
     @right = nil
+    @depth = depth
   end
 
   def leaf?
--- test/node_test.rb
+++ test/node_test.rb
@@ -12,4 +12,38 @@ class NodeTest < Minitest::Test
     assert_instance_of Node, node
   end
 
+  def test_know_its_a_leaf
+    node = Node.new
+
+    assert node.leaf?
+  end
+
+  def test_knows_its_not_a_leaf
+    node = Node.new
+    node.left = Node.new
+    node2 = Node.new
+    node2.right = Node.new
+    node3 = Node.new
+
+    refute node.leaf?
+    refute node2.leaf?
+    assert node3.leaf?
+  end
+
+  def test_starts_at_depth_0
+    node = Node.new
+
+    assert_equal 0, node.depth
+  end
+
+  def test_can_be_at_another_depth
+    node = Node.new(42, "life")
+
+    assert_equal 0, node.depth
+
+    node.depth = 4
+
+    assert_equal 4, node.depth
+  end
+
 end
