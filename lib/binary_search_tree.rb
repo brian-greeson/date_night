d318e35edbef2f280e2e29b37230481e23850ec5
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-01-30 19:16:56 -0700

    Add it_can_create_a_node test

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -2,12 +2,13 @@ class BinarySearchTree
 
   def initialize()
     @nodes = {}
+    @current_depth = 0
   end
 
   def insert(score = 0, title = "")
     @nodes[score] = Node.new(score, title)
-
-
+    # do future magic here
+    @current_depth
   end
 
 end
