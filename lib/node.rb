650b615a0f5cb242a1b88b5edecc96b1595f2ea1
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-02-01 09:55:58 -0700

    Update setter for run_recursion to set all children

--- lib/node.rb
+++ lib/node.rb
@@ -11,12 +11,23 @@ class Node
     @run_recursive = false
   end
 
+  def run_recursive=(choice)
+    @run_recursive = choice
+    @child_left.run_recursive = choice if @child_left
+    @child_right.run_recursive = choice if @child_right
+  end
+
+  def set_entire_tree_recursion
+    self.run_recursive = @run_recursive
+  end
+
   def leaf?
     !(@child_left || @child_right) #No NOR comparison in ruby :(
   end
 
   def insert(score_to_find, title)
     if @run_recursive
+      self.set_entire_tree_recursion
       inserted_node = insert_recursive(score_to_find, title)
       inserted_node.depth
     else
@@ -27,11 +38,13 @@ class Node
 
   def depth_of(score_to_find)
     if @run_recursive
+      self.set_entire_tree_recursion
       depth_of_recursive(score_to_find)
     else
       depth_of_iterative(score_to_find)
     end
   end
+
 
   # ******************* SEARCHING *******************
 
@@ -54,8 +67,19 @@ class Node
     return current_depth
   end
 
-  def depth_of_recursive(score_to_find)
+  def depth_of_recursive(score_to_find, current_depth = 0)
+    # return nil if current_node == nil
+    binding.pry
+    if score_to_find == @score
+      return current_depth
+    end
 
+    if score_to_find < @score
+      @child_left.depth_of_recursive(score_to_find, current_depth + 1)
+    else
+      @child_right.depth_of_recursive(score_to_find, current_depth + 1)
+    end
+    return current_depth
   end
 
 
@@ -88,6 +112,12 @@ class Node
   end
 
   def insert_recursive(score_to_find, title, current_depth = 0)
+    if @title.empty? #Is this the first insert?
+      @title = title
+      @score =  score_to_find
+      @depth = current_depth
+      return self
+    end
 
     if score_to_find < self.score
       @child_left ? @child_left.insert_recursive(score_to_find, title, current_depth + 1) : @child_left = Node.new(score_to_find, title, current_depth)
--- test/node_test.rb
+++ test/node_test.rb
@@ -61,7 +61,19 @@ class NodeTest < Minitest::Test
 
     assert_equal 1, node.depth_of(92)
     assert_equal 2, node.depth_of(50)
+  end
 
+  def test_can_find_depth_of_node_recursively
+    node = Node.new()
+    node.run_recursive = true
+
+    node.insert(61, "Bill & Ted's Excellent Adventure")
+    node.insert(16, "Johnny English")
+    node.insert(92, "Sharknado 3")
+    node.insert(50, "Hannibal Buress: Animal Furnace")
+
+    assert_equal 1, node.depth_of(92)
+    assert_equal 2, node.depth_of(50)
   end
 
 
